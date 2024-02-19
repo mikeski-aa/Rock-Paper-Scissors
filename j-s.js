c53d8d94e721a01a5961ebdcbe2dbf0cc9a16e8c
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-02-19 23:29:45 +0100

    Button functionality added to tally scores
    Added functionality to add and tally score when pressing buttons.
    Also added functionality of the bottom button to restart the game when pressed by user, resetting banner and score

--- j-s.js
+++ j-s.js
@@ -24,100 +24,112 @@ LOOP back to start
 */
 
 
+//set starting score of the game
+let playerScoreR = 0;
+let computerScoreR = 0;
+
+
+//listens for click event, depending on target clicked it executes that button
 document.addEventListener("DOMContentLoaded", () => {
+
+    const btnDel = document.querySelector('#deleteScore');
     let btn = document.querySelector('.buttons');
+    const playerScoreDiv = document.querySelector('.playerScore');
+    const computerScoreDiv = document.querySelector('.computerScore');
+    const banner = document.querySelector('h1');
+
     btn.addEventListener('click', (event) => {
         let target = event.target;
 
         switch(target.id){
             case 'playerRock':
                 console.log('Player chose Rock');
-                getComputerChoice()
                 playerSelection = 'rock';
-                playRound(playerSelection, savedComputerSelection);
+                playRound(playerSelection);
                 console.log(playerScoreR, computerScoreR);
+                scoreTally();
+                winCondition();
                 break;
 
             case 'playerPaper':
                 console.log('Player chose Paper');
-                getComputerChoice()
                 playerSelection = 'paper';
-                playRound(playerSelection, savedComputerSelection);
+                playRound(playerSelection);
                 console.log(playerScoreR, computerScoreR);
+                scoreTally();
+                winCondition();
                 break;
 
             case 'playerScissors':
                 console.log('Player chose Scissors');
-                getComputerChoice()
                 playerSelection = 'scissors';
-                playRound(playerSelection, savedComputerSelection);
+                playRound(playerSelection);
                 console.log(playerScoreR, computerScoreR);
+                scoreTally();
+                winCondition();
                 break;
         }
     });
+
+
+    function scoreTally() {
+        playerScoreDiv.textContent = playerScoreR;
+        computerScoreDiv.textContent = computerScoreR;
+    }
+
+    btnDel.addEventListener('click', () => {
+        playerScoreDiv.textContent = "";
+        computerScoreDiv.textContent = "";
+        banner.textContent = "Let's play: rock, paper, scissors!"
+        playerScoreR = 0;
+        computerScoreR = 0;
+    })
+    // check for win condition, change banner text
+    function winCondition(){
+        
+        if (playerScoreR === 5 && computerScoreR === 5){
+            banner.textContent = 'WE HAVE A TIE!';
+        } else if (playerScoreR === 5 && computerScoreR < 5) {
+            banner.textContent = 'YOU WIN!';
+        } else if (playerScoreR < 5 && computerScoreR === 5) {
+            banner.textContent = 'COMPUTER WINS!'; 
+        }
+    };
+
+    // computer decides on their choice from array
+    function getComputerChoice(){
+        let choiceArray = [
+            'rock',
+            'paper',
+            'scissors'
+        ];
+
+        let choiceIndex = Math.floor(Math.random() * choiceArray.length);
+        let computerSelection = choiceArray[choiceIndex];
+        console.log('The computer has selected ', computerSelection);
+        savedComputerSelection = computerSelection;
+        return savedComputerSelection;
+    }
+
+
+
+    // compare user input and computer choice, return results, add tally to the score
+    function playRound(playerSelection){
+        getComputerChoice();
+        if (playerSelection === savedComputerSelection){
+            console.log('We have a tie, you both selected ', playerSelection);
+            return playerScoreR += 1, computerScoreR += 1;
+        } else if (playerSelection === 'rock' && savedComputerSelection === 'paper') {
+            console.log('Paper beats rock - you LOSE!');
+            return computerScoreR += 1;
+        } else if (playerSelection === 'scissors' && savedComputerSelection === 'rock'){
+            console.log('Rock beats scissors - you LOSE!');
+            return computerScoreR += 1;
+        } else if (playerSelection === 'paper' && savedComputerSelection === 'scissors') {
+            console.log('Scissors beats paper - you LOSE!');
+            return computerScoreR += 1;
+        } else return playerScoreR += 1;
+
+        
+    };
 });
-
-let playerScoreR = 0;
-let computerScoreR = 0;
-
-
-function tallyScore(){
-    
-    while (playerScore > 5 && computerScore > 5){
-        player
-    }
-}
-
-
-
-// computer decides on their choice from array
-function getComputerChoice(){
-    let choiceArray = [
-        'rock',
-        'paper',
-        'scissors'
-    ];
-
-    let choiceIndex = Math.floor(Math.random() * choiceArray.length);
-    let computerSelection = choiceArray[choiceIndex];
-    console.log('The computer has selected ', computerSelection);
-    savedComputerSelection = computerSelection;
-    return savedComputerSelection;
-}
-
-
-
-// compare user input and computer choice, return results
-function playRound(playerSelection, savedComputerSelection){
-    
-    if (playerSelection === savedComputerSelection){
-        console.log('We have a tie, you both selected ', playerSelection);
-        return playerScoreR += 1, computerScoreR += 1;
-    } else if (playerSelection === 'rock' && savedComputerSelection === 'paper') {
-        console.log('Paper beats rock - you LOSE!');
-        return computerScoreR += 1;
-    } else if (playerSelection === 'scissors' && savedComputerSelection === 'rock'){
-        console.log('Rock beats scissors - you LOSE!');
-        return computerScoreR += 1;
-    } else if (playerSelection === 'paper' && savedComputerSelection === 'scissors') {
-        console.log('Scissors beats paper - you LOSE!');
-        return computerScoreR += 1;
-    } else return playerScoreR += 1;
-
-    
-}
-
-/*play 5 rounds of the game
-function playGame(){
-    for (let i = 0; i < 4; i++){
-
-    // ask for user input, convert to upper case for later comparison
-    let inputUser = prompt('Please type either rock, paper or scissors');
-    let playerSelection = inputUser.toUpperCase();
-
-    getComputerChoice();
-    playRound(playerSelection, savedComputerSelection);
-    
-    }
-}*/
-    
